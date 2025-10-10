4a9a478c991c826ba3d3ed562b744abe4c3260fb
roopsila <roopamjain1029@gmail.com>
2025-10-10 11:41:25 +0530

    LCS.c

---
+++ LCS.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#define Max 1000
+int longestCommonSubsequence(char* text1, char* text2) {
+    int m = strlen(text1);
+    int n = strlen(text2);
+    int dp[Max + 1][Max + 1]; 
+ 
+
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (text1[i-1] == text2[j-1])
+                dp[i][j] = dp[i-1][j-1] + 1;
+            else
+                dp[i][j] = (dp[i-1][j] > dp[i][j-1]) ? dp[i-1][j] : dp[i][j-1];
+        }
+    }
+
+    return dp[m][n];  
+}
